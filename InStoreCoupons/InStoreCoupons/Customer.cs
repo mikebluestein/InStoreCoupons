3107bed4e4c307cb8d7236b6911b960f78a51140
Mike Bluestein <mike.bluestein@gmail.com>
2013-11-12 13:25:52 -0500

    first commit

---
+++ InStoreCoupons/InStoreCoupons/Customer.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace InStoreCoupons
+{
+    public class Customer
+    {
+        public Customer ()
+        {
+        }
+
+        public int Id {
+            get;
+            set;
+        }
+
+        public string FirstName {
+            get;
+            set;
+        }
+
+        public string LastName {
+            get;
+            set;
+        }
+
+        public override string ToString ()
+        {
+            return string.Format ("[Customer: Id={0}, FirstName={1}, LastName={2}]", Id, FirstName, LastName);
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+In Store Coupons Example
+========================
+
+This project shows how to use iOS 7 iBeacons with Multipeer Connectivity.
+
+
+License:
+
+This code is licensed under the terms of the MIT/X11 license.
